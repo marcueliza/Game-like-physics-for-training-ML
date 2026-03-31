acf80799c1a7d7913df3e8aa0938cda7e8e3cf31
Vlasie <vlasie.ciobanu04@gmail.com>
2026-03-31 15:45:52 +0300

    Simulare adaugata

--- src/main/java/ro/tuiasi/edu/Proiect_PIP/App.java
+++ src/main/java/ro/tuiasi/edu/Proiect_PIP/App.java
@@ -1,13 +1,16 @@
 package ro.tuiasi.edu.Proiect_PIP;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.JFrame;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+		
+        JFrame frame = new JFrame("Simulator pendul");
+        frame.add(new Simulare());
+        frame.setSize(800, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
